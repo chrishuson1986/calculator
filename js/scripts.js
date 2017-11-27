f2c7a120c59e83a538d744d01ac9be87be03bae3
Chris Huson and omar solano <omar@>
2017-11-27 15:13:22 -0800

    add addition, subtraction, multiplication, division

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,37 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-alert(add(number1, number2));
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var remainder = function(number1, number2) {
+  return number1 % number2;
+};
+
+var bmi = function(number1, number2) {
+  return number1 / (number2/number2);
+};
+
+
+
+
+
+
+
+
+
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+
+
+alert(multiply(number1, number2));
